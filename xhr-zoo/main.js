8c0c683f60367b45def57fc340d9141d752dc106
Mary Alice Orr <m.a.orr17@gmail.com>
2018-03-26 21:19:58 -0500

    added js

--- xhr-zoo/main.js
+++ xhr-zoo/main.js
@@ -1,1 +1,37 @@
-console.log("hello");
+
+
+const printToDom = (string, id) => {
+    document.getElementById(id).innerHTML = string;
+};
+
+
+
+const buildDomString = (animalArray) => {
+    let domString = "";
+    for (let i=0; i<animalArray.length; i++) {
+        domString += `<h2>${animalArray[i].names}</h2>`
+    }
+    printToDom(domString, "animal-holder");
+};
+
+function executeThisCodeifXHRFails() {
+    console.log("this failed - try again");
+};
+
+function executeThisCodeAfterFileLoaded() {
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.animals);
+};
+
+
+
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+    myRequest.addEventListener("error", executeThisCodeifXHRFails);
+    myRequest.open("GET", "animals.json");
+    myRequest.send();
+};
+
+startApplication ();
